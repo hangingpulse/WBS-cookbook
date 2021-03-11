f535f84fda80591469bc29b50596e179aa058779
Jon Tokugawa <79482239+Joldon@users.noreply.github.com>
2021-03-11 17:36:16 +0100

    Share button

--- src/app.js
+++ src/app.js
@@ -1,3 +1,22 @@
+
+
+// nShare button
+
+
+
+const shareButton = document.querySelector("share");
+console.log(shareButton);
+
+$( document ).ready(function() {
+    //custom button for homepage
+    $( ".share-btn" ).click(function(e) {
+         $('.networks-5').not($(this).next( ".networks-5" )).each(function(){
+            $(this).removeClass("active");
+        });
+        $(this).next( ".networks-5" ).toggleClass( "active" );
+   });   
+});
+
 // Content of the Recipe Slot that loads as the default (curently Arrabiata recipe)
 
 $(document).ready(() => {
